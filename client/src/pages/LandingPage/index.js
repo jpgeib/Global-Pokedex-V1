14e2fe950cb2052c2a44bec839b5695205fd697a
James Geib <jpgeib2@gmail.com>
2021-05-11 11:39:33 -0400

    created links for each pokemon generation in LandingPage and tested routes

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class LandingPage extends Component {
     render() {
@@ -8,6 +9,34 @@ class LandingPage extends Component {
                 <Grid.Row>
                     <Grid.Column>
                         <Header as="h1">Welcome to Global Pokédex! - Please select a generation.</Header>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Link to="/geni">Generation I</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/genii">Generation II</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/geniii">Generation III</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/geniv">Generation IV</Link>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Link to="/genv">Generation V</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/genvi">Generation VI</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/genvii">Generation VII</Link>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <Link to="/genviii">Generation VIII</Link>
                     </Grid.Column>
                 </Grid.Row>
             </>
