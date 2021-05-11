ef04e3190f423bd2788ffc53cf35dfea75e68112
James Geib <jpgeib2@gmail.com>
2021-05-11 11:31:04 -0400

    created routes for Gens 1-5 pages in App container

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import LandingPage from "../../pages/LandingPage";
+import FirstGen from "../../pages/FirstGen";
+import SecondGen from "../../pages/SecondGen";
+import ThirdGen from "../../pages/ThirdGen";
+import FourthGen from "../../pages/FourthGen";
+import FifthGen from "../../pages/FifthGen";
 
 import "./style.css";
 
@@ -11,6 +16,11 @@ class App extends Component {
             <>
                 <Grid>
                     <Route exact path="/" render={() => <LandingPage />} />
+                    <Route exact path="/geni" render={() => <FirstGen />} />
+                    <Route exact path="/genii" render={() => <SecondGen />} />
+                    <Route exact path="/geniii" render={() => <ThirdGen />} />
+                    <Route exact path="/geniv" render={() => <FourthGen />} />
+                    <Route exact path="/genv" render={() => <FifthGen />} />
                 </Grid>
             </>
         );
